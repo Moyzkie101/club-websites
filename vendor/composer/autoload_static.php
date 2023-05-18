7a9ae47c880e187a2e15a5ff24ff57a60e068ac2
Moyzkie101 <102865569+Moyzkie101@users.noreply.github.com>
2023-05-18 12:37:35 +0800

    update google/apiclient 2.0 lib

--- vendor/composer/autoload_static.php
+++ vendor/composer/autoload_static.php
@@ -59,8 +59,8 @@ class ComposerStaticInitc4a86cb956ee87f8b353516eb5aa5c87
         ),
         'Psr\\Http\\Message\\' => 
         array (
-            0 => __DIR__ . '/..' . '/psr/http-message/src',
-            1 => __DIR__ . '/..' . '/psr/http-factory/src',
+            0 => __DIR__ . '/..' . '/psr/http-factory/src',
+            1 => __DIR__ . '/..' . '/psr/http-message/src',
         ),
         'Psr\\Http\\Client\\' => 
         array (
